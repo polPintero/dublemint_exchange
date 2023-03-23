533efe1233e76fcb60e8477d39317da1bee84b3f
Dmitriy <johndoe@example.com>
2023-03-23 16:04:13 +0200

    styled dropdown

--- src/components/DropdownComp.vue
+++ src/components/DropdownComp.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="dropdown" @click.stop="toggleVisible">
-    <button class="dropdown__btn">{{ modelValue }}</button>
-    <ul class="dropdown__content" :class="{'dropdown__content--show': visible}">
+    <div class="dropdown__wrap-btn">
+      <IconComp name="ChevronIconComp" class="dropdown__chevron"></IconComp>
+      <button class="dropdown__btn">{{ modelValue }}</button>
+    </div>
+    <ul
+      class="dropdown__content"
+      :class="{'dropdown__content--show': visible}"
+      @click="toggleVisible"
+    >
       <template v-for="item in list" :key="item.name">
         <li
           class="dropdown__content__item"
+          :class="{'dropdown__content__item--active': item.name === modelValue}"
           :value="item.name"
           v-if="conditionVisible(item)"
           @click="$emit('update:modelValue', item.name)"
         >
-          {{ item.name }}
+          <span>{{ item.name }}</span>
         </li>
       </template>
     </ul>
@@ -17,8 +25,15 @@
 </template>
 
 <script>
+import IconComp, { ChevronIconComp } from '@/components/IconsVue'
+
 export default {
   name: 'DropdownComp',
+  components: {
+    IconComp,
+    /* eslint-disable vue/no-unused-components */
+    ChevronIconComp
+  },
   emits: ['update:modelValue'],
   props: {
     list: {
@@ -34,18 +49,18 @@ export default {
     },
     conditionVisible: {
       type: Function,
-      default() {
+      default () {
         return () => true
       }
     }
   },
-  data() {
+  data () {
     return {
       visible: false
     }
   },
   methods: {
-    toggleVisible() {
+    toggleVisible () {
       this.visible = !this.visible
       this.visible
         ? document.addEventListener('click', this.toggleVisible)
@@ -59,34 +74,67 @@ export default {
 .dropdown {
   position: relative;
   display: inline-block;
+  cursor: pointer;
+
   &__btn {
-    background-color: #3498db;
-    color: white;
-    padding: 16px;
-    font-size: 16px;
-    border: none;
+    padding: var(--gap) var(--gap-double);
+    padding-right: calc(var(--gap-double) * 4);
+    font-size: 1rem;
+    color: inherit;
     cursor: pointer;
-    &:hover,
+    border: 2px solid transparent;
+    background: linear-gradient(180deg, #414344 0%, #2b2d2e 100%);
+    box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.4),
+      inset 0px 1px 0px rgba(255, 255, 255, 0.3);
+
     &:focus {
-      background-color: #2980b9;
+      border: 2px solid #e0f97d;
+      filter: drop-shadow(0px 0px 4px #92d300);
     }
+  }
+
+  &__wrap-btn {
+    position: relative;
+  }
+
+  .dropdown__chevron {
+    position: absolute;
+    right: 5px;
+    top: 50%;
+    width: 1rem;
+    height: 1rem;
+    transform: translateY(-50%);
+    z-index: 1;
   }
 
   &__content {
     display: none;
     position: absolute;
-    background-color: #f1f1f1;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-    z-index: 1;
+    margin-block-start: 5px;
+    padding-left: 0;
+    right: 0;
+    background: linear-gradient(180deg, #4f5254 0%, #393b3a 97.3%);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25),
+      inset 0px 1px 0px rgba(255, 255, 255, 0.3);
+    z-index: 999999;
 
     &__item {
-      color: black;
-      padding: 12px 16px;
+      padding: var(--gap) var(--gap-double);
       text-decoration: none;
       display: block;
+      border-bottom: 1px solid #2a2b2d;
       &:hover {
-        background-color: #ddd;
+        background: linear-gradient(180deg, #2c2e2f 0%, #404142 100%);
+        box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.3),
+          inset 0px 2px 3px rgba(0, 0, 0, 0.5);
+        span {
+          color: #eeffab;
+          text-shadow: 0px 0px 8px rgba(146, 211, 0, 0.8);
+        }
+      }
+      &--active {
+        color: #eeffab;
+        text-shadow: 0px 0px 8px rgba(146, 211, 0, 0.8);
       }
     }
 
