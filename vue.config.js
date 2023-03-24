0209a98582e00a99e6f68b9064326393c835d07f
Dmitriy <johndoe@example.com>
2023-03-24 12:50:22 +0200

    try public

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/gh_bublic/',
   css: {
     loaderOptions: {
       sass: {
